85de2c30a206e3bacf047877124669c9fd404641
Tanjim <tanjimseo@gmail.com>
2023-10-24 00:42:55 +0530

    Added register controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,1 +1,41 @@
-export const register = () => { }
+import { hashPassword } from "../helpers/authHelper.js";
+import userModel from "../models/userModel.js";
+
+export const register = async (req, res) => {
+    try {
+        const { name, email, password, phone, address } = req.body;
+        //validation
+        if (!name || !email || !password || !phone || !address) {
+            return res.send({ error: "All fields are requred" })
+        }
+
+        //check user
+        const exisitingUser = await userModel.findOne({ email });
+
+        //exisiting user
+        if (exisitingUser) {
+            res.status(500).send({
+                success: true,
+                message: "Already Register please login",
+            })
+        }
+
+        //register user
+
+        const hashedPassword = await hashPassword(password);
+        //save
+        const user = await new userModel({ name, email, phone, address, password: hashedPassword }).save()
+        res.status(200).send({
+            success: true,
+            message: "User Register Successfully",
+            user
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success: false,
+            message: "Error in registeration",
+            error
+        })
+    }
+}
--- server/helpers/authHelper.js
+++ server/helpers/authHelper.js
@@ -4,12 +4,12 @@ export const hashPassword = async (password) => {
     try {
         const saltRounds = 10;
         const hashedPassword = await bcrypt.hash(password, saltRounds);
-        return hashPassword;
+        return hashedPassword;
     } catch (error) {
         console.log(error)
     }
 }
 
-export const camparePassword = async (password, hashPassword) => {
-    return bcrypt.compare(password, hashPassword)
+export const camparePassword = async (password, hashedPassword) => {
+    return bcrypt.compare(password, hashedPassword)
 }
