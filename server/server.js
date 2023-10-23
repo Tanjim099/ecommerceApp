cb25c1a0e20e208bcd0476e5fa5e1b5037df36f6
Tanjim <tanjimseo@gmail.com>
2023-10-23 23:35:21 +0530

    setup simple server

---
+++ server/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+
+
+//configure env
+dotenv.config();
+
+
+//db config
+connectDB()
+
+//rest object
+const app = express();
+
+//middelwares
+app.use(express.json());
+app.use(morgan("dev"));
+
+//rest api
+app.get("/", (req, res) => {
+    res.send({
+        message: "welcome to ecommerce app"
+    })
+})
+
+//port
+const PORT = process.env.PORT || 8080;
+
+//run listen
+app.listen(PORT, () => {
+    console.log(`Server Running on ${PORT}`.bgCyan.white)
+})
