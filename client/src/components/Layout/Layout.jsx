a33439d0a4d0e06b5adec6d971444aebd48a40f0
Tanjim <tanjimseo@gmail.com>
2023-10-24 23:31:57 +0530

    Added helmet

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,9 +1,16 @@
 import Footer from "./Footer"
-import Header from "./Header"
+import Header from "./Header";
+import { Helmet } from "react-helmet";
 
-function Layout({ children }) {
+function Layout({ children, title, description, keywords, author }) {
     return (
         <div>
+            <Helmet>
+                <meta name="description" content={description} />
+                <meta name="keywords" content={keywords} />
+                <meta name="author" content={author} />
+                <title>{title}</title>
+            </Helmet>
             <Header />
             <main style={{ minHeight: "75vh" }}>
                 {children}
@@ -11,6 +18,13 @@ function Layout({ children }) {
             <Footer />
         </div>
     )
+};
+
+Layout.defaultProps = {
+    title: "Home Page",
+    description: "Mern Stack Project",
+    keywords: "React, Node, Express, MongoDB",
+    author: "Tanjim"
 }
 
 export default Layout
